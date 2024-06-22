7cc896f7e08df65f1a82cd9dc9dfa866049a6693
Ryoris <c22.dubreuil@gmail.com>
2024-06-22 19:27:13 +0200

    ajout projet2

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.querySelectorAll(".carousel img");
+    if (n >= slides.length) {slideIndex = 0}
+    if (n < 0) {slideIndex = slides.length - 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slides[slideIndex].classList.add("active");
+}
+
